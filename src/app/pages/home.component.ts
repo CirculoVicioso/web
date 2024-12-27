784aba4b454678f749aef6754435833a378a0ffb
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-27 13:46:49 -0300

    [#14] - Agrega links de navegación al home

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
+import { LinkCard, LinkCardComponent } from '../components/link-card/link-card.component'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    LinkCardComponent
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    @for(link of links; track $index){
+      <a app-link-card [content]="link"></a>
+    }
   `,
-  styles: ``
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+      height: 100%;
+      padding: 16px;
+    }`
 })
 export class HomeComponent {
+  links: LinkCard[] = [
+    {
+      title: 'Último capítulo',
+      backgroundImageUrl: 'card-background.png'
+    },
+    {
+      title: 'RRSS',
+      backgroundImageUrl: 'card-background.png'
+    },
+    {
+      title: 'Tienda',
+      backgroundImageUrl: 'card-background.png'
+    },
+    {
+      title: 'Wiki',
+      backgroundImageUrl: 'card-background.png'
+    },
+    {
+      title: 'Búsqueda episodios',
+      backgroundImageUrl: 'card-background.png'
+    },
 
+  ]
 }
