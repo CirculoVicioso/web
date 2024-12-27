ed2847939249fe275a0ea0d12acda234764709cb
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-27 13:08:29 -0300

    [#12] - Agrega estilos para footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,11 +5,39 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: `
-    <p>
-      footer works!
-    </p>
+    <img class="logo" [src]="'logo-footer.svg'" height="48" width="48">
+    <ul class="links">
+      <li>> acerca_de</li>
+      <li>> privacidad</li>
+    </ul>
   `,
-  styles: ``
+  styles: `
+    :host {
+      align-items: center;
+      display: flex;
+      height: 91px;
+      justify-content: space-between;
+    }
+
+    .links {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 8px;
+
+      li {
+        list-style-type: none;
+        color: #000;
+        text-align: center;
+        font-family: Arial;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: -0.4px;
+      }
+    }`
 })
 export class FooterComponent {
 
