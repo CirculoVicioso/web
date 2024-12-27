f9f9b096770f164d78a686a45b05d3d57e899ba7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-27 13:46:18 -0300

    [#14] - Agrega componente LinkCardComponent

---
+++ src/app/components/link-card/link-card.component.ts
@@ -0,0 +1,60 @@
+import { Component, input } from '@angular/core';
+
+export interface LinkCard {
+  title: string;
+  backgroundImageUrl: string;
+}
+
+@Component({
+  selector: '[app-link-card]',
+  imports: [],
+  template: `
+    <span class="title">
+      {{ content().title }}
+    </span>
+  `,
+  styles: `
+    :host {
+      display: flex;
+      width: 100%;
+      justify-content: space-around;
+      align-items: center;
+      backdrop-filter: blur(2px);
+      flex: 1 1 0;
+
+      &:first-of-type {
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.24) 0%, rgba(0, 0, 0, 0.24) 100%), linear-gradient(0deg, rgba(238, 45, 111, 0.20) 0%, rgba(238, 45, 111, 0.20) 100%), url('/card-background.png') lightgray 50% / cover no-repeat;
+      }
+
+      &:nth-of-type(2) {
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.24) 0%, rgba(0, 0, 0, 0.24) 100%), linear-gradient(0deg, rgba(73, 116, 230, 0.20) 0%, rgba(73, 116, 230, 0.20) 100%), url('/card-background.png') lightgray 50% / cover no-repeat;
+      }
+
+      &:nth-child(3) {
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.24) 0%, rgba(0, 0, 0, 0.24) 100%), linear-gradient(0deg, rgba(230, 185, 73, 0.20) 0%, rgba(230, 185, 73, 0.20) 100%), url('/card-background.png') lightgray 50% / cover no-repeat;
+      }
+
+      &:nth-child(4) {
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.24) 0%, rgba(0, 0, 0, 0.24) 100%), linear-gradient(0deg, rgba(73, 230, 87, 0.20) 0%, rgba(73, 230, 87, 0.20) 100%), url('/card-background.png') lightgray 50% / cover no-repeat;
+      }
+
+      &:nth-child(5) {
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.24) 0%, rgba(0, 0, 0, 0.24) 100%), linear-gradient(0deg, rgba(145, 130, 94, 0.20) 0%, rgba(145, 130, 94, 0.20) 100%), url('/card-background.png') lightgray 50% / cover no-repeat;
+      }
+    }
+
+    .title {
+      color: #FFF;
+      text-align: center;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: -1.2px;
+      text-transform: uppercase;
+    }
+  `
+})
+export class LinkCardComponent {
+  content = input.required<LinkCard>()
+}
