1f161ed411d4f1ba2cc9e860fd324ff998b2543a
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-27 12:34:39 -0300

    [#12] - Importa header y footer en AppComponent. Agrega layout básico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [ RouterOutlet, HeaderComponent, FooterComponent ],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>`,
+  styles: `
+    :host {
+      display: flex;
+      flex-direction: column;
+      height: 100svh;
+      padding: 16px;
+    }`
 })
 export class AppComponent {
   title = 'cv-web';
