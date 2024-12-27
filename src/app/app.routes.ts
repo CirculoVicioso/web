da860c18868720c5d1476ed169700b868b7d93fa
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-12-27 13:47:30 -0300

    [#14] - Agrega redirección a home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home.component').then(m => m.HomeComponent) },
+  { path: '**', redirectTo: '' }
 ];
